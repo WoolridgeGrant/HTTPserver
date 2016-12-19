abdb6812ccb99ad9801b0a146ae77cf3e357a9c8
Woolridgegrant <porteedm@gmail.com>
2016-12-19 20:16:10 +0100

    Refactoring addLog

--- src/log.c
+++ src/log.c
@@ -17,7 +17,6 @@
 /*Creer tmp*/
 void addLog(requete *req){
   char log[500], size[50], pid[50], tid[20], ip[20];
-  char n[2] = "\n";
   time_t rawtime;
   struct tm *timeinfo;
   int i;
@@ -28,7 +27,7 @@ void addLog(requete *req){
 
   /*msg*/
   i = 0;
-  while(req->msg[i] != '\n'  && req->msg[i] != '\r'){ /*Appelez moi detective Conan : C'est une sequence CRLF (\r suivi de \n) qui separe les lignes*/
+  while(req->msg[i] != '\n'  && req->msg[i] != '\r'){ /*C'est une sequence CRLF (\r CARRIAGE RETURN suivi de \n LINE FEED) qui separe les lignes*/
     log[i] = req->msg[i];
     i++;
   }
@@ -39,12 +38,10 @@ void addLog(requete *req){
   sprintf(size, "%d", req->size_file);
   strcat(log, size);
 
-
   /*http_code*/
   printf("http_code : %s\n", req->http_code);
   strcat(log, ", http_code : ");
   strcat(log, req->http_code);
-
 
   /*ip clt*/
   printf("ip : %s\n", inet_ntoa(req->sin.sin_addr));
@@ -52,13 +49,11 @@ void addLog(requete *req){
   sprintf(ip, "%s", inet_ntoa(req->sin.sin_addr));
   strcat(log, ip);
 
-
   /*pid server*/
   printf("pid server : %d\n", getpid());
   strcat(log, ", PID server : ");
   sprintf(pid, "%d", getpid());
   strcat(log, pid);
-
 
   /*tid*/
   printf("tid : %ld\n", (long)pthread_self());
@@ -66,34 +61,24 @@ void addLog(requete *req){
   sprintf(tid, "%ld", (long)pthread_self());
   strcat(log, tid);
 
-
   /*date et heure*/
   time ( &rawtime );
   timeinfo = localtime ( &rawtime );
   strcat(log, ", time : ");
   strcat(log, asctime (timeinfo));
-  printf("date : %s\n", asctime (timeinfo));
-
+  printf("date : %s", asctime (timeinfo)); /*Ajoute /n*/
 
   /*ecrire dans le fichier de log*/
-
   pthread_mutex_lock(&fd_log_mutex);
   i = 0;
   while(log[i] != '\0'){
-    if(log[i] != '\n' && write(fd_log, &log[i], sizeof(char)) < 0){
+    if(write(fd_log, &log[i], sizeof(char)) < 0){
       perror("Erreur d'ecriture dans le fichier de log\n");
       /*return errno;*/
     }
     i++;
   }
-
-  if(write(fd_log, &n, sizeof(char)) < 0){
-    perror("Erreur d'ecriture dans le fichier de log\n");
-    /*return errno;*/
-  }
-
-
   pthread_mutex_unlock(&fd_log_mutex);
 
-  printf("\n-> log ajouté\n");
+  printf("Log ajouté.\n");
 }
